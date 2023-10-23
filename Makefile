657a49c343718332b6ba114f740496e1a9539f46
Eugenio Tampieri <eugenio@eutampieri.eu>
2023-10-23 22:14:46 +0200

    Demo: show hello message

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ protobuf:
 	gcc -std=c99 -Wall protobuf/encode.c protobuf/tag.c protobuf/varint.c
 bep:
 	gcc -std=c99 -Wall bep/hello.c
-build: protobuf bep
-	echo ""
+build:
+	gcc -std=c99 -Wall main.c bep/*.c protobuf/*.c -lc
 clean:
 	rm a.out
 
--- main.c
+++ main.c
@@ -1,25 +1,22 @@
 #include "protobuf/encode.h"
 #include "protobuf/varint.h"
+#include "bep/hello.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
 #include <string.h>
+#define __USE_XOPEN_EXTENDED
+#include <unistd.h>
 
 int main() {
     char* buffer = malloc(PROTOBUF_DEFAULT_BUFFER_LENGTH);
-    list_t v = {.value =  "Hello, World!\n"};
-    v.length = strlen(v.value);
-    varint_t n = proto_varint_new();
-    n.int32 = UINT16_MAX;
-    uintptr_t encoded = proto_encode(buffer,
-                                     PROTOBUF_DEFAULT_BUFFER_LENGTH,
-                                     1,
-                                     /*1,
-                                     LEN,
-                                     v,*/
-                                     2,
-                                     VARINT, n);
+    char* hostname = malloc(PROTOBUF_DEFAULT_BUFFER_LENGTH);
+    gethostname(hostname, PROTOBUF_DEFAULT_BUFFER_LENGTH);
+    bep_hello_t hello = {.device_name = hostname, .client_version="0.1", .client_name="cyncthing"};
+    uintptr_t encoded = bep_hello_encode(&hello, buffer, PROTOBUF_DEFAULT_BUFFER_LENGTH);
     for(uintptr_t i = 0; i < encoded; i++) {
         printf("%c", buffer[i]);
     }
+    free(buffer);
+    free(hostname);
 }
