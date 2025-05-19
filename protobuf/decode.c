5230a46eb8b4fb81b935773ced0bd6f78e47702b
Eugenio Tampieri <eugenio@eutampieri.eu>
2025-05-19 11:52:45 +0200

    Test int32 encoding

---
+++ protobuf/decode.c
@@ -0,0 +1,5 @@
+//
+// Created by eugenio on 25/10/23.
+//
+
+#include "decode.h"
---
+++ protobuf/decode.h
@@ -0,0 +1,8 @@
+//
+// Created by eugenio on 25/10/23.
+//
+
+#ifndef CYNCTHING_DECODE_H
+#define CYNCTHING_DECODE_H
+
+#endif //CYNCTHING_DECODE_H
